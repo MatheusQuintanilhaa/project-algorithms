f994dc81aa4cd85460f8a791103d978b87167a1d
Matheus <matheussantos.quintanilha@gmail.com>
2023-07-06 16:02:52 -0300

    req 5 feito

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,16 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(nums, list) or any(not isinstance(num, int) for num in nums):
+        return False
+
+    counter = {}
+
+    for num in nums:
+        if num <= 0:
+            continue
+
+        if num in counter:
+            return num
+        else:
+            counter[num] = 1
+
+    return False
