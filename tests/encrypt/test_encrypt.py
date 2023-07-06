dfb996c9b69e7b6e313530255df8a86d4cab2b6e
Matheus <matheussantos.quintanilha@gmail.com>
2023-07-06 17:47:55 -0300

    agora vamos linter

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -5,20 +5,13 @@ from challenges.challenge_encrypt_message import encrypt_message
 def test_encrypt_message():
     pass
 
-# Teste para chave ímpar
+
 assert encrypt_message("message", 3) == "sem_egas"
-# assert encrypt_message("trybe", 5) == "ebyrt"
 
-
-# Teste para chave par
 assert encrypt_message("message", 4) == "ega_ssem"
-# assert encrypt_message("trybe", 2) == "eybtr"
 
-# Teste para chave inválido
 assert encrypt_message("message", -2) == "egassem"
-# assert encrypt_message("trybe", 8) == "ebyrt"
 
-# Teste para tipos incorretos de entrada
 with pytest.raises(TypeError, match="tipo inválido para message"):
     encrypt_message(5, 3)
 
